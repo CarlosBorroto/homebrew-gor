56416fcceeb316ba599d6add134300c712a31e9b
sigmar <sigmar@wuxinextcode.com>
2021-05-12 13:42:50 +0000

    update gorspark

--- gorpipe.rb
+++ gorpipe.rb
@@ -1,7 +1,7 @@
 class Gorpipe < Formula
   desc ""
   homepage "https://github.com/gorpipe/gor"
-  url "https://github.com/gorpipe/gor-spark/releases/download/v1.3.8/gor-spark-3.10.2-dist.zip"
+  url "https://github.com/gorpipe/gor-spark/releases/download/v1.3.8/gorshell-3.10.2-dist.zip"
   sha256 "09dd1e319294db0feb9b0434d102a3c2f83feda339a66d7afe06a061efb7251b"
   version "3.10.2"
 
