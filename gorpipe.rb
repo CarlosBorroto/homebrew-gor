7add6ed6cdaf16722ebc6a4306a99d36528bc46d
sigmar <sigmar@wuxinextcode.com>
2021-05-17 19:36:35 +0000

    update spark

--- gorpipe.rb
+++ gorpipe.rb
@@ -1,9 +1,9 @@
 class Gorpipe < Formula
   desc ""
   homepage "https://github.com/gorpipe/gor"
-  url "https://github.com/gorpipe/gor-spark/releases/download/v1.3.8/gorshell-3.10.2-dist.zip"
-  sha256 "09dd1e319294db0feb9b0434d102a3c2f83feda339a66d7afe06a061efb7251b"
-  version "3.10.2"
+  url "https://github.com/gorpipe/gor-spark/releases/download/v1.3.8/gorshell-3.10.3-dist.zip"
+  sha256 "1dbdcd4c7172f3fa8b892ee3c2fb311df370c170034ef2fbbcddeb2602044b03"
+  version "3.10.3"
 
   def install
     prefix.install Dir["*"]
